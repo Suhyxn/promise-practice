6f8e8e722b1a11183ac77a51a7627ac980b83149
Suhyxn <momentomori21489@gmail.com>
2022-06-14 18:54:55 +0900

    resolve, then 이용해 보기

--- main.js
+++ main.js
@@ -1,80 +1,60 @@
-// function a(callback) {
-//   setTimeout(()=> {
-//     console.log('A')
-//     callback()
-//   }, 1000)
-// }
-
-// function b(callback) {
-//   setTimeout(()=> {
-//     console.log('B')
-//     callback()
-//   }, 1000)
-// }
-
-// a(function () {
-//   b(function () {
-//     console.log('Done!')
-//   })
-// })
-
 function a() {
-  // promise: 약속의 객체!
-  return new Promise(function (resolve) {
-    setTimeout(function () {
+  return new Promise((resolve) => {
+    setTimeout(() => {
       console.log('A')
-      resolve('Hello A')
+      resolve() 
     }, 1000)
   })
 }
 
-// function b() {
-//   console.log('B')
-// }
-
-// async function test() {
-//   const res = await a()
-//   console.log('res:', res)
-//   b()
-// }
-
-// test()
-
 function b() {
-  // promise: 약속의 객체!
-  return new Promise(function (resolve) {
-    setTimeout(function () {
+  return new Promise((resolve) => {
+    setTimeout(() => {
       console.log('B')
-      resolve('Hello B')
+      resolve() 
     }, 1000)
   })
 }
 
 function c() {
-  // promise: 약속의 객체!
-  return new Promise(function (resolve) {
-    setTimeout(function () {
+  return new Promise((resolve) => {
+    setTimeout(() => {
       console.log('C')
-      resolve('Hello C')
+      resolve() 
     }, 1000)
   })
 }
 
 function d() {
-  // promise: 약속의 객체!
-  return new Promise(function (resolve) {
-    setTimeout(function () {
+  return new Promise((resolve) => {
+    setTimeout(() => {
       console.log('D')
-      resolve('Hello D')
+      resolve() 
     }, 1000)
   })
 }
 
-async function test() {
-  await a()
-  await b()
-  await c()
-  await d()
-  console.log('Done!')
+// 비추천
+// function test() {
+//   a().then(() => {
+//     b().then(() => {
+//       c().then(() => {
+//         d().then(() => {
+//           console.log('Done!')
+//         })
+//       })
+//     })
+//   })
+// }
+// test() 
+
+function test() {
+  a()
+    .then(() => b())
+    .then(() => c())
+    .then(() => d())
+    .then(() => {
+    console.log('Done!')
+  })
 }
 test()
